b2c7f885176b3d3a76ef8a415414a8b24e2fd3ff
huongnv <huongnv@tohsoft.com>
2021-09-09 17:34:41 +0700

    add file

--- api-install.sh
+++ api-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 apt update
-apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
-apt-get install python3-pip python3-dev nginx
-apt install python3-venv
+apt install -y python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+apt-get install -y python3-pip python3-dev nginx
+apt install -y python3-venv
 mkdir /etc/openvpn/
 cd /etc/vpnapiproject/ || return
 python3 -m venv vpnapiprojectenv

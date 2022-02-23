f66abe2ebbdb208b91df451f443c9e2a7655c4aa
DuongVu <vudt@apero.vn>
2022-02-23 11:38:07 +0700

    fix lại cài plugin cho python

--- api-install.sh
+++ api-install.sh
@@ -3,6 +3,28 @@ apt update
 yes | apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
 yes | apt-get install python3-pip python3-dev nginx
 yes | apt install python3-venv
+chmod 777 -Rv /var/log/openvpn/status.log
+cd /etc/openvpn || return
+wget https://raw.githubusercontent.com/huongnv251291/easyrsa/main/count_user/countuser.py -O /etc/openvpn/countuser.py
+chmod 777 -Rv /etc/openvpn/countuser.py
+touch /etc/openvpn/countuser.sh
+chmod +x /etc/openvpn/countuser.sh
+echo "#!/bin/bash
+python3 -i  /etc/openvpn/countuser.py" >>/etc/openvpn/countuser.sh
+cat >/etc/systemd/system/countuser.service <<EOF
+[Unit]
+Description=CountUser Service
+
+[Service]
+ExecStart=/etc/openvpn/countuser.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl enable countuser
+systemctl start countuser
+cd
 mkdir -p /root/vpnapiproject
 cd /root/vpnapiproject/ || return
 python3 -m venv vpnapiprojectenv

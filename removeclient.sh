3cbcb6bd534fb2859fa9f1eb4c61663602c7c9a6
DuongVu <vudt@apero.vn>
2022-04-12 17:10:53 +0700

    update removeclient.sh

--- removeclient.sh
+++ removeclient.sh
@@ -20,8 +20,8 @@ if [[ $CLIENTEXISTS != '1' ]]; then
 else
   cd /etc/openvpn/easy-rsa/ || return
 fi
-echo "yes" | ./easyrsa revoke "$CLIENT"
-./easyrsa gen-crl
+./easyrsa --batch revoke "$CLIENT"
+EASYRSA_CRL_DAYS=3650 ./easyrsa gen-crl
 numberInIndex=$(grep -n "/CN=$CLIENT\$" /etc/openvpn/easy-rsa/pki/index.txt)
 echo "$numberInIndex"
 if [[ $numberInIndex =~ ^[[:digit:]:R] ]]; then
@@ -32,20 +32,21 @@ if [[ $numberInIndex =~ ^[[:digit:]:R] ]]; then
   grep -v "/CN=$CLIENT\$" /etc/openvpn/easy-rsa/pki/index.txt >temp && mv temp /etc/openvpn/easy-rsa/pki/index.txt
   rm -rf /etc/openvpn/crl.pem
   cp /etc/openvpn/easy-rsa/pki/crl.pem /etc/openvpn/crl.pem
-  chmod 0664 /etc/openvpn/crl.pem
+  chmod 664 /etc/openvpn/crl.pem
+  sed -i "/^$CLIENT,.*/d" /etc/openvpn/ipp.txt
   # Finally, restart and enable OpenVPN
-  if [[ $OS == 'arch' || $OS == 'fedora' || $OS == 'centos' || $OS == 'oracle' ]]; then
-    systemctl stop openvpn-server@server
-    systemctl start openvpn-server@server
-  elif [[ $OS == "ubuntu" ]] && [[ $VERSION_ID == "16.04" ]]; then
-    # On Ubuntu 16.04, we use the package from the OpenVPN repo
-    # This package uses a sysvinit service
-    systemctl stop openvpn
-    systemctl start openvpn
-  else
-    systemctl stop openvpn@server
-    systemctl start openvpn@server
-  fi
+#  if [[ $OS == 'arch' || $OS == 'fedora' || $OS == 'centos' || $OS == 'oracle' ]]; then
+#    systemctl stop openvpn-server@server
+#    systemctl start openvpn-server@server
+#  elif [[ $OS == "ubuntu" ]] && [[ $VERSION_ID == "16.04" ]]; then
+#    # On Ubuntu 16.04, we use the package from the OpenVPN repo
+#    # This package uses a sysvinit service
+#    systemctl stop openvpn
+#    systemctl start openvpn
+#  else
+#    systemctl stop openvpn@server
+#    systemctl start openvpn@server
+#  fi
   echo "revoke $CLIENT success"
 else
   echo "revoke fail check profile name $CLIENT"
